021724c26b8eaf318099e340a8e4f61edb5b1922
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2023-05-07 16:34:30 -0300

    commit

--- aula1/js/script.js
+++ aula1/js/script.js
@@ -1,1 +1,2 @@
 alert('Olá mundo 2!')
+
---
+++ aula2/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("texto").innerHTML="Meu primeiro texto em <strong>JS</strong>!";
+alert(10+5)
+
+console.log('Esse é um console.log')
